3260ff41d9ceeb724401cf2b8fa6279a1cfadb27
Henrik Kronborg <henrkron@stud.ntnu.no>
2019-02-20 10:24:44 +0100

    Fikset mellomrom

--- gui.py
+++ gui.py
@@ -2,24 +2,27 @@ from tkinter import *
 from PIL import ImageTk, Image
 from tkinter.filedialog import askopenfilename
 
+from image_reader import *
+
 def openFile():
-	filename = askopenfilename()
-	print(filename)
-	return filename;
+    filename = askopenfilename()
+    print(filename)
+    return filename
 
 def gui():
-	root = Tk()
-	root.title("EiT - Maskinlæring, Gruppe 1")
+    root = Tk()
+    root.title("EiT - Maskinlæring, Gruppe 1")
 
-	img = ImageTk.PhotoImage(Image.open("cat.jpg"))
+    img = ImageTk.PhotoImage(Image.open("1968.png"))
 
-	panel = Label(root, image=img)
-	panel.pack()
+    panel = Label(root, image=img)
+    panel.pack()
 
-	button = Button(root, text="hei", width=25, command=openFile)
-	button.pack()
+    button = Button(root, text="hei", width=25, command=openFile)
+    button.pack()
 
-	mainloop()
+    mainloop()
 
 if __name__ == '__main__':
+    ARRAY_FROM_PATH("1968.png")
     gui()
