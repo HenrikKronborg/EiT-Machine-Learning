4b662812ed8cc924c771c91ecc6c90957dec8fea
simentha <simentha@stud.ntnu.no>
2019-03-20 14:32:48 +0100

    vgg

---
+++ VGG.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 27 14:50:08 2019
+
+@author: simen
+"""
+
+import keras
+import tensorflow as tf
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, InputLayer
+from keras.applications.vgg16 import VGG16
+
+def createModel(opt, loss, in_shape, num_classes):
+    # Create an instance of the VGG_model without FC network
+    vgg_model = VGG16(include_top = False, weights = 'imagenet', input_shape = in_shape)
+    layer_dict = dict([(layer.name, layer) for layer in vgg_model.layers])    
+
+    x = layer_dict['block5_pool'].output
+
+    # Stacking a new fully connected network on top of it    
+    x = Flatten()(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    x = Dense(num_classes, activation='softmax')(x)
+
+    custom_model = keras.Model(input = vgg_model.input, output = x)
+
+    # Only the last layers should be retrained
+    for layer in custom_model.layers[:-4]:
+        layer.trainable = False
+    
+    custom_model.compile(opt, loss, metrics = ['accuracy'])
+
+    return custom_model
---
+++ VGG_main.py
@@ -0,0 +1,51 @@
+from random import randint
+from dataset_loader import data
+from VGG import createModel
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import keras
+
+# load training data and convert to RGB
+train_data = data["training_set"]
+train_labels = data["training_labels"]
+
+train_data = np.array(train_data)
+train_data = np.resize(train_data, (*train_data.shape, 1))
+train_data /= 255
+
+rgb_train = np.zeros((*train_data.shape[0:3],3))
+for i in range(train_data.shape[0]):
+    rgb_train[i] = np.concatenate([train_data[i],train_data[i],train_data[i]], axis=2)
+
+# convert labels to years and one hot encode
+ezyConvert = lambda x: x // 12
+train_labels = ezyConvert(train_labels)
+oneHot_labels = keras.utils.to_categorical(train_labels)
+
+# hyper parameters
+EPOCHS = 100
+BATCH_SIZE = 64
+IN_SHAPE = rgb_train[0].shape
+NUM_CLASSES = 20
+EARLY_STOPPING_PATIENCE = 4
+LEARNING_RATE = 0.001
+LAMBDA = 0
+
+loss = keras.losses.MSE
+opt = keras.optimizers.Adam(lr = LEARNING_RATE, decay = LAMBDA)
+
+# creates a model with the structure defined in VGG.py
+model = createModel(opt, loss, IN_SHAPE, NUM_CLASSES)
+early_stop = keras.callbacks.EarlyStopping(patience=EARLY_STOPPING_PATIENCE, restore_best_weights=True)
+
+# start training
+hist = model.fit(rgb_train, oneHot_labels, validation_split=0.1, epochs=EPOCHS,
+          batch_size=BATCH_SIZE, callbacks=[early_stop])
+          
+print("-----------    Training finished.    -----------")
+print(hist.history.val_acc)
+
+# save model.
+model.save("new_model.h5")
