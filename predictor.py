51ae38749184f840973b3e2da36342b119939c1b
John <johnew@stud.ntnu.no>
2019-03-27 14:00:37 +0100

    added confusion matrix

--- predictor.py
+++ predictor.py
@@ -11,19 +11,47 @@ from sklearn.metrics import confusion_matrix
 
 if __name__ == "__main__":
 
-    MODEL_NAME = "new_model.h5"
+    MODEL_NAME = "subSet-model.h5"
 
     model = load_model(MODEL_NAME)
+    print("data loaded.")
 
-    DATA_PATH = "test_set.npy"
-    LABEL_PATH = "test_labels.npy"
+    DATA_PATH = "2test_set.npy"
+    LABEL_PATH = "2test_labels.npy"
 
-    test = np.load(DATA_PATH)
-    test_labels = np.load(LABEL_PATH)
+    test = np.load(DATA_PATH)[100:]
+    test = np.resize(test, (len(test), 100, 100, 1))
+
+
+    test_labels = np.load(LABEL_PATH)[100:]
+    ezyConvert = lambda x: x // 12
+    test_labels = ezyConvert(test_labels)
+
 
     pred = model.predict(test)
 
-    cm = confusion_matrix(test, pred)
+    pred = np.argmax(pred, axis=1)
+
+    print("shape: ", pred.shape)
+    print("test shape: ", test_labels.shape)
+    print(test_labels)
+
+
+    #cm = confusion_matrix(test_labels, pred)
+
+    from pandas_ml import ConfusionMatrix
+    import matplotlib.pyplot as plt
+
+    print(test_labels[:10])
+    print(pred[:10])
+
+
+  
+    cm = ConfusionMatrix(test_labels, pred)
+    cm.plot()
+    plt.show()
+
+    cm.plot()
     
 
     ## predict the given data.
